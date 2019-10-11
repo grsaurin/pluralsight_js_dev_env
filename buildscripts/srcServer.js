04f99fda320653edf2091a10ca3e3b2e6ce31499
Gelson.saurin <gelson.saurin@salesforce.com>
2019-10-11 12:10:13 -0300

    mocking data configuratib

--- buildscripts/srcServer.js
+++ buildscripts/srcServer.js
@@ -17,9 +17,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    {"id":1,"firstName":"Bob", "lastName":"Smith", "email":"bob@gmmail.com"},
+    {"id":2,"firstName":"Tammy", "lastName":"Norton", "email":"tnorton@gmmail.com"},
+    {"id":3,"firstName":"Tina", "lastName":"Lee", "email":"lee.tina@gmmail.com"}
+  ]);
+});
+
 app.listen(port, (err) => {
   if(err){
-    console.log(err);
+    console.log(err); // eslint-disable-line no-console
   }
   else{
     open('http://localhost:' + port);
