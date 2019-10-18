0dba468ca3940a2730edad55589733c3d48f301c
Gelson Saurín <alb255@gmail.com>
2019-10-18 09:03:54 -0300

    mocking data configurations

--- buildscripts/generateMockData.js
+++ buildscripts/generateMockData.js
@@ -7,13 +7,14 @@
 import {jsf} from 'json-schema-faker';
 import {schema} from './mockDataSchema';
 import {fs} from 'fs';
+import chalk from 'chalk';
 
-const json = JSON.stringify(jsf(schema));
+const json = JSON.stringify(jsf.resolve(schema));
 
 fs.writeFile("./src/api/db.json", json, function (err) {
   if (err) {
-    return console.log(err); // eslint-disable-line no-console
+    return console.log(chalk-red(err)); // eslint-disable-line no-console
   } else {
-    console.log("Mock data generated."); // eslint-disable-line no-console
+    console.log(chalk.green("Mock data generated.")); // eslint-disable-line no-console
   }
 });
